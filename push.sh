985d36b01cb9db87d2546f332708032d76e63071
Lisii04 <2256106896@qq.com>
2024-03-12 00:11:16 +0800

    Update in 2024-03-12 00:11

--- push.sh
+++
@@ -1,9 +0,0 @@
-git reset .
-git add .
-if [ $1 ]
-then 
-    git commit -m "$1 in "$(date +%F)" "$(date +%R)
-else
-    git commit -m "Update in "$(date +%F)" "$(date +%R)
-fi
-git push --progress origin main
