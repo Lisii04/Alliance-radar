de89bf2aa6018aeb58c373a691bab19d59370972
Lisii04 <2256106896@qq.com>
2024-03-19 18:07:38 +0800

    --progress in 2024-03-19 18:07

---
+++ radar_ws/build/radar_serial/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/workspaces/Alliance-radar/radar_ws/install/serial/share/serial/package.sh"
---
+++ radar_ws/build/ros_tcp_endpoint/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/workspaces/Alliance-radar/radar_ws/install/ros_tcp_endpoint'
